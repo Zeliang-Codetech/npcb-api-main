c80aee85296158dae5181b2568c65a0c9481245c
HarshP42L <harshchourasia69@gmail.com>
2025-02-28 00:53:22 +0530

    Server issue resolved

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (corsOptions.origin.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', 'https://npcb.in');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
